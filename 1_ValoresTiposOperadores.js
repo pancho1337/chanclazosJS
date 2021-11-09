43e43391a9ea52fd79384bb3e95619e1645ef83e
Francisco Rojas <pancho404x@gmail.com>
2021-11-09 17:59:27 -0600

    Create 1_ValoresTiposOperadores.js

---
+++ 1_ValoresTiposOperadores.js
@@ -0,0 +1,26 @@
+// Primitive
+  // Boolean
+console.log(true)
+  // Null
+console.log(null)
+  // Undefined
+console.log(undefined)
+  // Number
+console.log(1337)
+  // String 
+console.log("yo soy panchoDelRancho")
+  // Symbol
+console.log(Symbol("dosDeOros"))
+
+// Object
+  // Array
+console.log([true, null])
+  // Object 
+console.log({nombre: "valor"})
+  // Function
+console.log(function(){})
+  // Date
+console.log(new Date)
+  // Regex
+console.log(/pancho/)
+console.log(new RegExp('pancho'))
